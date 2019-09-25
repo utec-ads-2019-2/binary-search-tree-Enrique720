d0df3788222e9f5a5a3f6b700a3be3fa15bf1eb6
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-25 01:39:19 -0500

    avance

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
-#include "tester/tester.h"
-
+//#include "tester/tester.h"
+#include "bstree.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "===========================================================" << endl;
-    cout << "Binary Search Tree Practice" << endl;
-    cout << "===========================================================" << endl << endl;
-
-    Tester::execute();
-    return EXIT_SUCCESS;
+   // cout << "===========================================================" << endl;
+   // cout << "Binary Search Tree Practice" << endl;
+   // cout << "===========================================================" << endl << endl;
+//
+   // Tester::execute();
+   // return EXIT_SUCCESS;
+    BSTree<int> a;
+    a.insert(4);
+    a.insert(7);
+    a.insert(2);
+    a.insert(1);
+    a.insert(8);
+    a.insert(9);
+    a.insert(18);
+    a.insert(6);
+    a.insert(3);
+    a.insert(12);
+    a.traverseInOrder();
 }
