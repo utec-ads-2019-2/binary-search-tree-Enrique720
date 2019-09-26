5d125ee8af739f162e392b9ca7b9cae75e54b354
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-26 12:34:08 -0500

    Update iterator.h

--- iterator.h
+++ iterator.h
@@ -2,39 +2,70 @@
 #define ITERATOR_H
 
 #include "node.h"
-
-template <typename T> 
+#include <stack>
+#include <stdexcept>
+using namespace std;
+template <typename T>
 class Iterator {
     private:
         Node<T> *current;
-
+        stack <Node<T>*> nodos;
     public:
-        Iterator() {
-            // TODO
-        }
+        Iterator():current{nullptr} {}
 
-        Iterator(Node<T> *node) {
-            // TODO
+        Iterator(Node<T> *node,bool be): current{node} {
+            if(!be){
+                while(node){
+                    nodos.push(node);
+                    node=node->left;
+                }
+            }
+            else
+            {
+                while(node){
+                    node=node->right;
+                }
+            }
+            this->current=node;
+
         }
 
         Iterator<T>& operator=(const Iterator<T> &other) {          
-            // TODO
+            this->current = other.current;
+            return *this;
         }
 
         bool operator!=(Iterator<T> other) {
-            // TODO
+            return current != other.current;
         }
 
         Iterator<T>& operator++() {
-            // TODO
+            auto * temp=nodos.top();
+            nodos.pop();
+            if(temp->right){
+                nodos.push(temp->right);
+                while(nodos.top()->left){
+                    nodos.push(nodos.top()->left);
+                }
+            }
+            if(!nodos.empty())
+                current=nodos.top();
+            else
+                current=nullptr;
+
+            return *this;
         }
 
-        Iterator<T>& operator--() {
+        /*Iterator<T>& operator--() {
             // TODO
-        }
+        }*/
 
         T operator*() {
-            // TODO
+            if(current != nullptr){
+            return current->data;}
+            else{
+                throw out_of_range("Passed index!");
+            }
         }
 };
 
